cf7c633f9d279cc63af10e62ae59f54dd1be4884
Abdullah ai ifaque <mdabdullah200211@gmail.com>
2025-02-02 01:32:02 +0600

    Add files via upload

--- Recovering a Small String.cpp
+++ Recovering a Small String.cpp
@@ -1,1 +1,25 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main(){
 
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        if(28>=n){
+            ll p=(n-2)+96;
+            cout<<"a"<<"a"<<(char)p<<endl;
+        }
+        else if(n>28 && n<=53){
+            ll p=(n-27)+96;
+            cout<<"a"<<(char)p<<"z"<<endl;
+        }
+         else if(n>53 && n<=78){
+            ll p=(n-52)+96;
+            cout<<(char)p<<"z"<<"z"<<endl;
+        }
+    }
+
+}
