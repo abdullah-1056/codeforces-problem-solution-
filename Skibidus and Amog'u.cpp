11a33354ff876b03f485e883ee5c16eae6229b2b
Abdullah ai ifaque <mdabdullah200211@gmail.com>
2025-02-10 21:10:57 +0600

    Add files via upload

---
+++ Skibidus and Amog'u.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main(){
+   ll t;
+   cin>>t;
+   while(t--){
+     string s;
+     cin>>s;
+
+     ll p=s.size();
+
+                for(ll i=0;i<p-2;i++){
+                   cout<<s[i];
+                }
+                cout<<"i"<<endl;
+
+        }
+
+   }
+
