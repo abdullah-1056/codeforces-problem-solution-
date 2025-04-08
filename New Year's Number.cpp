61eb2bdbe00dff54d435d4cb94606439b5bd14d7
Abdullah ai ifaque <mdabdullah200211@gmail.com>
2025-04-08 14:44:27 +0600

    Add files via upload

---
+++ New Year's Number.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+       ll n;
+       cin>>n;
+
+      if(n%2020<=n/2020){
+          cout<<"YES"<<endl;
+       }
+      else {
+          cout<<"NO"<<endl;
+       }
+    }
+
+       }
+
+
+
