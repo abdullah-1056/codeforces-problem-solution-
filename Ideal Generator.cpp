b239bfe2b88e258caebe1c6f94532a6d68726114
Abdullah ai ifaque <mdabdullah200211@gmail.com>
2025-04-18 17:18:37 +0600

    Add files via upload

---
+++ Ideal Generator.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+
+    ll t;
+    cin>>t;
+    while(t--){
+
+        ll k;
+        cin>>k;
+
+        if(k%2==0){
+            cout<<"NO"<<endl;
+        }else{
+            cout<<"YES"<<endl;
+        }
+    }
+}
